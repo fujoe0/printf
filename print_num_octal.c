8d17ae4b988d4614406651aa04d3b9002979c239
Adegbiteoluwaseun1990 <adegbiteoluwaseun99@gmail.com>
2023-09-22 06:42:39 -0700

    This file contains print_num_octal functions

---
+++ print_num_octal.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * prinnoct - print the number in octal begining with zero
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int prinnoct(va_list arguments, char *buf, unsigned int ibuf)
+{
+	int input, i, isnegative, count, first_digit;
+	char *octal, *bin;
+
+	input = va_arg(arguments, int);
+	isnegative = 0;
+	if (input == 0)
+	{
+		ibuf = handl_buf(buf, '0', ibuf);
+		return (1);
+	}
+	if (input < 0)
+	{
+		input = (input * -1) - 1;
+		isnegative = 1;
+	}
+	ibuf = handl_buf(buf, '0', ibuf);
+	bin = malloc(sizeof(char) * (32 + 1));
+	bin = fill_binary_array(bin,input, isnegative, 32);
+	octal = malloc(sizeof(char) * (11 + 1));
+	octal = fill_oct_array(bin, octal);
+	for (first_digit = i = count = 0; octal[i]; i++)
+	{
+		if (octal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = handl_buf(buf, octal[i], ibuf);
+			count++;
+		}
+	}
+	free(bin);
+	free(octal);
+	return (count + 1);
+}
+
