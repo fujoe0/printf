4022c5ac4556e4610f4b9893e8d934d200f0742f
fujoe0 <joesanni7@gmail.com>
2023-09-21 17:14:09 -0700

    variable declaration error

--- ev_print_function.c
+++ ev_print_function.c
@@ -1,52 +1,51 @@
 #include "main.h"
 
 /**
- * _printf - formatted output conversion and print data.
- * @format: input string.
- *
- * Return: number of chars printed.
+ * ev_print_func - returns the amount of identifiers.
+ * @s: argument indentifier
+ * @index: index of argument identifier.
+ * Return: amount of identifiers.
  */
-int _printf(const char *format, ...)
+int ev_print_func(const char *s, int index)
 {
-	unsigned int a = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
+	print_t pr[] = {
+		{"c", print_chr}, {"s", print_str}, {"i", print_int},
+		{"d", print_int}, {"b", print_bnr}, {"u", print_unt},
+		{"o", print_oct}, {"x", print_hex}, {"X", print_upx},
+		{"S", print_usr}, {"p", print_add}, {"li", prinlint},
+		{"ld", prinlint}, {"lu", prinlunt}, {"lo", prinloct},
+		{"lx", prinlhex}, {"lX", prinlupx}, {"hi", prinhint},
+		{"hd", prinhint}, {"hu", prinhunt}, {"ho", prinhoct},
+		{"hx", prinhhex}, {"hX", prinhupx}, {"#o", prinnoct},
+		{"#x", prinnhex}, {"#X", prinnupx}, {"#i", print_int},
+		{"#d", print_int}, {"#u", print_unt}, {"+i", prinpint},
+		{"+d", prinpint}, {"+u", print_unt}, {"+o", print_oct},
+		{"+x", print_hex}, {"+X", print_upx}, {" i", prinsint},
+		{" d", prinsint}, {" u", print_unt}, {" o", print_oct},
+		{" x", print_hex}, {" X", print_upx}, {"R", print_rot},
+		{"r", print_rev}, {"%", print_prg}, {"l", print_prg},
+		{"h", print_prg}, {" +i", prinpint}, {" +d", prinpint},
+		{"+ i", prinpint}, {"+ d", prinpint}, {" %", print_prg},
+		{NULL, NULL},
+	};
+	int i = 0, j = 0, first_index;
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[a] == '%' && !format[a + 1]))
-		return (-1);
-	if (!format[a])
-		return (0);
-	for (a = 0; format && format[a]; a++)
+	first_index = index;
+	while (pr[i].type_arg)
 	{
-		if (format[a] == '%')
+		if (s[index] == pr[i].type_arg[j])
 		{
-			if (format[a + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
-			}
+			if (pr[i].type_arg[j + 1] != '\0')
+				index++, j++;
 			else
-			{	function = get_print_func(format, a + 1);
-				if (function == NULL)
-				{
-					if (format[a + 1] == ' ' && !format[a + 2])
-						return (-1);
-					handl_buf(buffer, format[a], ibuf), len++, a--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					a += ev_print_func(format, a + 1);
-				}
-			} a++;
+				break;
 		}
 		else
-			handl_buf(buffer, format[a], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
+		{
+			j = 0;
+			i++;
+			index = first_index;
+		}
 	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
+	return (j);
 }
-
